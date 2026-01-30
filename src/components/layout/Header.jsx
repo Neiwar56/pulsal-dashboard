896f9ccac6f4b3daea7ae44fdde6d0395eacde48
Neiwar56 <harryawoudo@gmail.com>
2026-01-30 15:47:42 +0100

    edition dashboard

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -29,7 +29,7 @@ const Header = ({ title = "Vue d'ensemble" }) => {
             <p className="text-xs text-gray-500">Admin</p>
           </div>
           <div className="w-10 h-10 rounded-full bg-primary/10 flex items-center justify-center text-primary font-bold">
-            TA
+            NH
           </div>
         </div>
       </div>
